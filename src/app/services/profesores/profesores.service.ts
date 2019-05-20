399d27db57337dab13c6a84ec3e6b2b31c0ff4e6
camilo2412 <42158460+camilo2412@users.noreply.github.com>
2019-05-19 19:53:03 -0500

    Ajuste del Servicio de Profesores
    Barrido de código inecesarios

--- src/app/services/profesores/profesores.service.ts
+++ src/app/services/profesores/profesores.service.ts
@@ -1,17 +1,13 @@
 import { Injectable } from '@angular/core';
-import { AuthService } from '../auth/auth.service';
-import * as firebase from 'firebase';
-
 import { AngularFireDatabase } from '@angular/fire/database';
-import { Docente } from 'src/app/models/docente.interface';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfesoresService {
 
-  constructor(private authService: AuthService,
-              private afd: AngularFireDatabase) {
+  constructor(private afd: AngularFireDatabase) {
 
   }
 
