4acacdee2b56b2fbaf6eb43a74111dadd09e786c
Hector Franco <hector-franco@upc.edu.co>
2019-05-20 11:16:45 -0500

    Generar Horario

--- src/app/components/horario/horario.component.ts
+++ src/app/components/horario/horario.component.ts
@@ -5,6 +5,7 @@ import { AngularFireDatabase } from '@angular/fire/database';
 import * as firebase from 'firebase';
 import { Docente } from 'src/app/models/docente.interface';
 import { ProfesoresService } from 'src/app/services/profesores/profesores.service';
+import { Materia } from 'src/app/models/materia.interface';
 
 @Component({
   selector: 'app-horario',
@@ -16,14 +17,30 @@ export class HorarioComponent implements OnInit {
   profesor = firebase.auth().currentUser;
   nombre: string;
   disponible: boolean;
+  cHorario: boolean;
+  vHorario: boolean;
+  ambos = false;
   mensajeDisponibilidad: string;
 
-  constructor(private materiasService: MateriasService,
-              private authService: AuthService,
-              private profeService: ProfesoresService,
-              private afd: AngularFireDatabase) {
+  getMateria: boolean;
+  error: boolean;
+  agregar: boolean;
+  materia: Materia[];
 
-        this.nombre = this.profesor.displayName;
+  creditos: number;
+
+  horario: any[];
+
+  codigoMateria: string;
+  dias = ['Lunes', 'Martes'];
+  horas = ['8:00', '12:00'];
+
+  constructor(private materiasService: MateriasService,
+    private authService: AuthService,
+    private profeService: ProfesoresService,
+    private afd: AngularFireDatabase) {
+
+    this.nombre = this.profesor.displayName;
   }
 
   ngOnInit() {
@@ -35,18 +52,91 @@ export class HorarioComponent implements OnInit {
 
   isDisponble() {
     this.profeService.isAvailable(this.profesor.uid)
-    .then((profesor: Docente) => {
-      this.disponible = profesor[2];
-      if (this.disponible) {
-        this.mensajeDisponibilidad = 'Aun no tiene un horario asignado. Puede hacerlo aquí';
-      } else {
-        this.mensajeDisponibilidad = 'Horario asignado';
-      }
-    })
-    .catch((error) => console.log(error));
+      .then((profesor: Docente) => {
+        this.disponible = profesor[2];
+        if (this.disponible) {
+          this.mensajeDisponibilidad = 'Aun no tiene un horario asignado. Puede hacerlo aquí';
+        } else {
+          this.mensajeDisponibilidad = 'Horario asignado';
+        }
+      })
+      .catch((error) => console.log(error));
   }
 
-  
+  verMateriaCarreraID(carrera: string, materiaID: string) {
+    this.materiasService.getMateria(carrera, materiaID)
+      .then((materia: any[]) => {
+        if (materia.length !== 0) {
+          this.getMateria = true;
+          this.materia = materia;
+          this.agregar = false;
+          this.creditos = materia[1];
+          this.codigoMateria = materia[0];
+          console.log(this.creditos);
+        } else {
+          this.error = true;
+          this.getMateria = false;
+          this.agregar = false;
+        }
+      })
+      .catch((error) => console.log(error));
+  }
 
+  verMateriaID(materiaID: string) {
+    this.materiasService.getMateriasPrograma(materiaID)
+      .then((materia: any[]) => {
+        if (materia.length !== 0) {
+          this.getMateria = true;
+          this.materia = materia;
+          this.agregar = false;
+          this.creditos = materia[1];
+          this.codigoMateria = materia[0];
+        } else {
+          this.error = true;
+          this.getMateria = false;
+          this.agregar = false;
+        }
+      })
+      .catch((error) => console.log(error));
+  }
+
+  agregarMateria() {
+    this.agregar = true;
+    this.generarHorario(this.dias, this.horas);
+  }
+
+  crearHorario() {
+    this.cHorario = true;
+    this.vHorario = false;
+  }
+  verHorario() {
+    this.cHorario = false;
+    this.vHorario = true;
+  }
+
+
+  generarHorario(diasS: string[], horas: string[]) {
+
+    const horarios = [];
+    const dias = [];
+
+    for (let i = 0; i < diasS.length; i++) {
+      const materia = {
+        dia: diasS[i],
+        hora: horas[i]
+      };
+      dias.push(materia);
+    }
+
+    console.log(dias);
+    console.log(horarios);
+
+    firebase.database().ref('/profesores/' + this.profesor.uid + '/horarios/' + this.codigoMateria)
+      .set({
+        dias
+      })
+      .then(() => console.log('Horario Creado'))
+      .catch((error) => console.log(error));
+  }
 
 }
